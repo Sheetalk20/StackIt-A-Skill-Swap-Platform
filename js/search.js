aa15d69a2ad70153f6b2291075c0fa8c98c8ad00
Sheetal kumari <154450181+Sheetalk20@users.noreply.github.com>
2025-09-13 10:44:02 +0530

    Delete js directory

--- js/search.js
+++
@@ -1,9 +0,0 @@
-function searchSkills() {
-  const query = document.getElementById("skillSearch").value.toLowerCase();
-  const profiles = document.querySelectorAll(".profile-card");
-
-  profiles.forEach(profile => {
-    const skillText = profile.innerText.toLowerCase();
-    profile.style.display = skillText.includes(query) ? "block" : "none";
-  });
-}
